8731befd19973cc035878a135c2b3b21d02ccb02
jcdd <cami-516@hotmail.com>
2021-10-12 20:44:40 -0500

    calidad init

---
+++ src/test/java/edu/udem/com/taller1DevOpsCalidad/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udem.com.taller1DevOpsCalidad.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
