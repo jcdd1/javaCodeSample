b58ecac3cd0736a821ff2d9b6e64ca5e39c51dfc
Enrique de Jesús Durango Rivera <Edurango@intergrupo.com>
2021-10-13 21:47:25 -0500

    commit

--- src/main/java/edu/udem/com/taller1DevOpsCalidad/demo/DemoApplication.java
+++ src/main/java/edu/udem/com/taller1DevOpsCalidad/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package edu.udem.com.taller1DevOpsCalidad.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println(new OperationMath().sum(5, 8));
 	}
 
 }
